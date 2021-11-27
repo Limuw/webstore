6da653c3ec527bfecb6d9bb70f7ed6938ab10dd9
Limuw <itsLimuw@gmail.com>
2021-11-27 10:47:18 +0300

    tryout

--- js/webstore.js
+++ js/webstore.js
@@ -5,6 +5,12 @@ $('document').ready(function(){
 function loadgoods(){
     //загрузка товаров
     $.getJSON('goods.json',function(data){
-        console.log(data);
+        //console.log(data);
+        var out = '';
+        for (var key in data){
+            out +='<p>' + data [key] ['name'] + '<p>';
+            out +='<p>' + data [key] ['price'] + '<p>';
+        }
+        $('#goods').html(out);
     })
 }
