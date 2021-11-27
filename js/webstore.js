6253e7408ad2ea96dc6b7b1fb65dd85729477808
Limuw <itsLimuw@gmail.com>
2021-11-27 11:48:57 +0300

    changes

--- js/webstore.js
+++ js/webstore.js
@@ -1,20 +1,57 @@
-$('document').ready(function(){
-    loadgoods();
-})
+var cart = {}; //переменная корзины
 
-function loadgoods(){
-    //загрузка товаров
-    $.getJSON('goods.json',function(data){
+
+
+$('document').ready(function(){
+    loadGoods();
+    checkCart();
+    showMiniCart();
+});
+
+function loadGoods() {
+    //загрузка товаров на страницу
+    $.getJSON('goods.json', function (data) {
         //console.log(data);
         var out = '';
         for (var key in data){
-            out+='<div class = "single-goods">';
-            out+='<h3>' + data[key] ['name'] + '<h3>';
-            out+='<p>Цена: ' + data[key] ['price'] + '<p>';
+            out+='<div class="single-goods">';
+            out+='<h3>'+data[key]['name']+'</h3>';
+            out+='<p>Цена: '+data[key]['price']+'</p>';
             out+='<img src="'+data[key].image+'">';
-            out+='<button>Купить</button>'
+            out+='<button class="add-to-cart" data-art="'+key+'">Купить</button>';
             out+='</div>';
         }
         $('#goods').html(out);
-    })
+        $('button.add-to-cart').on('click', addToCart);
+    });
+}
+
+function addToCart() {
+    //добавление товара в крозину
+    var articul = $(this).attr('data-art');
+    if (cart[articul]!=undefined) {
+        cart[articul]++;
+    }
+    else {
+        cart[articul] = 1;
+    }
+    localStorage.setItem('cart', JSON.stringify(cart) );
+    //console.log(cart);
+    showMiniCart();
+}
+
+function checkCart(){
+    //проверка корзины в lStorage;
+    if ( localStorage.getItem('cart') != null) {
+        cart = JSON.parse (localStorage.getItem('cart'));
+    }
+}
+
+function showMiniCart(){
+    //содержимое корзины
+    var out ='';
+    for (var w in cart){
+        out += w + ' --- '+cart[w]+'<br>';
+    }
+    $('#mini-cart').html(out);
 }
